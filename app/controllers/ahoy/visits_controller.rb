86d5dd3feaf4c6eead128ab71f121971fb748a26
Darex TOSSA <gildasdarex@gmail.com>
2016-05-05 12:04:01 +0100

    test ahoy.user not nil

--- app/controllers/ahoy/visits_controller.rb
+++ app/controllers/ahoy/visits_controller.rb
@@ -2,6 +2,7 @@ module Ahoy
   class VisitsController < BaseController
     def create
       ahoy.track_visit
+      p ahoy.user
       render json: {visit_id: ahoy.visit_id, visitor_id: ahoy.visitor_id}
     end
   end
